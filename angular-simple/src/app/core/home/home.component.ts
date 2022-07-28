abe2ac3469c288f7f17a0682ca01b867c41cd8b2
oopsmails <oopsmails@gmail.com>
2022-07-28 16:40:04 -0400

    20220728: detect browser

--- angular-simple/src/app/core/home/home.component.ts
+++ angular-simple/src/app/core/home/home.component.ts
@@ -32,6 +32,8 @@ export class HomeComponent implements OnInit {
 
   public getScreenWidth: any;
   public getScreenHeight: any;
+  browserName = '';
+  browserVersion = '';
 
   navRoutes: any = null;
 
@@ -39,14 +41,16 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private router: Router
-  ) {}
-  
+  ) { }
+
   ngOnInit() {
     console.log('########### in HomeComponent, TRANSLATIONS_FR = \n' + JSON.stringify(TRANSLATIONS_FR));
     this.navRoutes = LOADING_STENCIL_ROUTES_URLS;
 
     this.getScreenWidth = window.innerWidth;
     this.getScreenHeight = window.innerHeight;
+    this.browserName = this.detectBrowserName();
+    this.browserVersion = this.detectBrowserVersion();
   }
 
   navToPage(page) {
@@ -73,7 +77,7 @@ export class HomeComponent implements OnInit {
   }
 
   valueEmittedFromChildComponent: string;
-  
+
   parentEventHandlerFunction(valueEmitted) {
     this.valueEmittedFromChildComponent = valueEmitted;
   }
@@ -82,6 +86,43 @@ export class HomeComponent implements OnInit {
   onWindowResize() {
     this.getScreenWidth = window.innerWidth;
     this.getScreenHeight = window.innerHeight;
+  }
+
+  detectBrowserName() {
+    const agent = window.navigator.userAgent.toLowerCase()
+    switch (true) {
+      case agent.indexOf('edge') > -1:
+        return 'edge';
+      case agent.indexOf('opr') > -1 && !!(<any>window).opr:
+        return 'opera';
+      case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
+        return 'chrome';
+      case agent.indexOf('trident') > -1:
+        return 'ie';
+      case agent.indexOf('firefox') > -1:
+        return 'firefox';
+      case agent.indexOf('safari') > -1:
+        return 'safari';
+      default:
+        return 'other';
+    }
+  }
+
+  detectBrowserVersion() {
+    var userAgent = navigator.userAgent, tem,
+      matchTest = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+    if (/trident/i.test(matchTest[1])) {
+      tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+      return 'IE ' + (tem[1] || '');
+    }
+    if (matchTest[1] === 'Chrome') {
+      tem = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+      if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    matchTest = matchTest[2] ? [matchTest[1], matchTest[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if ((tem = userAgent.match(/version\/(\d+)/i)) != null) matchTest.splice(1, 1, tem[1]);
+    return matchTest.join(' ');
   }
 
 }
